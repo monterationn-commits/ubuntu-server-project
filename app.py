f93ab52928c7c5cdab49e0d993146106b09a1396
monterationn-commits <monterationn@gmail.com>
2025-11-04 22:06:08 +0000

    first

--- app.py
+++ app.py
@@ -1,11 +1,1 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello from Ubuntu Server!"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+print('DONE')
